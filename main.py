736def670a486c91ba833368cb9260b6f7f6234a
Adrian Mülthaler <adrian.muelthaler@campus.lmu.de>
2024-10-23 00:31:11 +0200

    finished prompt pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ollama
 from collections import defaultdict
 import random
 import os
+from tqdm import tqdm
 
 SYSTEM_PROMPT = """
 You are an expert in object recognition.
@@ -45,16 +46,35 @@ def main(tgt_lan: str, concept: str, model: str):
     few_shots, forbidden_idx = format_few_shots(os.path.join("dataset", "en", f"{concept}.csv"), 30)
 
     with open(os.path.join("dataset", tgt_lan, f"{concept}.csv")) as f:
-        for l in f.readlines():
+        for l in tqdm(f.readlines(), desc=tgt_lan+" - "+concept):
             _id, entry = l.split(",")
             if _id in forbidden_idx:
                 continue
             p, a = entry.replace("Q: ", "").strip().split(" A: ")
             yield _id, p, prompt('llama3:instruct', few_shots, p), a
 
+def all_concepts(tgt_lan: str, model: str):
+
+    dir_name = os.path.join('results', model)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    dir_name = os.path.join(dir_name, tgt_lan)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+    for c in ['color', 'size', 'shape', 'height', 'material', 'mass', 'temperature', 'hardness']:
+        file_name = os.path.join(dir_name,c + ".txt")
+        if os.path.exists(file_name):
+            pritn("Hallo, das gibt es schon!")
+            continue
+        
+        for response in main(tgt_lan, c, model):
+            with open(file_name, 'a') as f:
+                f.write(str(response)+"\n")
+
 
 if __name__ == "__main__":
 
     # llama3:instruct, llava-llama3:latest 
-    for response in main("es", "mass", "llava-llama3:latest"):
-        print(response)
+    for l in ['ko', 'de', 'zh-CN', 'es', 'ja']:
+        all_concepts(tgt_lan=l, model="llama3:instruct")
